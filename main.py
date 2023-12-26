03e71e6afc156dc2c4ce0526a00882ece6428739
saardn <saard3123@gmail.com>
2023-12-26 18:03:19 +0200

    another commit

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-if 1 == 1:
-    print(1)
+if 2 == 2:
+    print(2)
